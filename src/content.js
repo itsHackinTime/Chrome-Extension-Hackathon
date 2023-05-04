dd0b2929bbec3f68b5fa1bc1922d39841f6f6dc6
Vander <vander@Vanders-MBP.attlocal.net>
2023-05-03 19:22:28 -0700

    progress on matrix extension

---
+++ src/content.js
@@ -0,0 +1,16 @@
+
+console.log('hello there');
+let square = document.createElement('div');
+square.style.backgroundColor = 'blue'
+square.style.zIndex = '15';
+square.style.height = '100rem'
+square.style.width = '100rem'
+square.style.position = 'absolute'
+square.style.top = '0px'
+square.style.left = '0px'
+square.style.opacity= .5;
+square.innerText = 'oiuwyerjhdsf'
+square.style.fontFamily = 'matrix font'
+square.style.color = 'green'
+square.style.fontSize = '50px'
+document.querySelector('body').appendChild(square);
